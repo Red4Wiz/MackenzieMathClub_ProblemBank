5d4bb4a02b681eac8a8820adc0bad24932574b13
Daniel Ye <danielyeqt@gmail.com>
2023-07-21 15:55:12 -0400

    Synchronize problem creation with backend

--- frontend/src/ProblemForm.js
+++ frontend/src/ProblemForm.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './ProblemForm.css';
+import {ROUTES} from './routes.js';
+import Select from 'react-select';
 
 const ProblemForm = () => {
+  // a total list of tags
+  const [problemTags, setProblemTags] = useState([])
+  const [contestTags, setContestTags] = useState([])
+  // selected tags
+  const [selectedPTags, setSelectedPTags] = useState([])
+  const [selectedCTags, setSelectedCTags] = useState([])
+
+  // get a list of possible tags
+  useEffect(() => {
+    // set problem tags
+    fetch(`${ROUTES.api}/problemTags`, {method: 'get'}).then((res) => {
+      if(res.status === 200){
+        res.json().then((value) => setProblemTags(value.map((el) => {return {"value": el.id, "label": el.name}})))
+      } else {
+        console.log("querying problem tags error")
+      }
+    })
+
+    // set contest tags
+    fetch(`${ROUTES.api}/contestTags`, {method: 'get'}).then((res) => {
+      if(res.status === 200){
+        res.json().then((value) => setContestTags(value.map((el) => {return {"value": el.id, "label": el.name}})))
+      } else {
+        console.log("querying contest tags error")
+      }
+    })
+  }, [])
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
+    // send a request
+    let url = ROUTES.api + "/problem/create/";
+    let data = new FormData(e.target);
+
+    fetch(url, {
+      method: "post",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+          'author': 1,
+          'title': data.get('title'),
+          'statement': data.get('statement'),
+          'solution': data.get('solution'),
+          'problemTags': selectedPTags.map((e) => e.value),
+          'contestTags': selectedCTags.map((e) => e.value)
+        })
+    }).then((res) => {
+      // if successful, then redirect
+      if(res.status === 200){
+        res.json().then((value) => {
+          console.log(value)
+        })
+      } else {
+        alert("Server Error!");
+      }
+    })
   };
 
   return (
@@ -25,11 +81,11 @@ const ProblemForm = () => {
         </label>
         <label>
           Problem Tags:
-          <input type="text" name="problemTags" />
+          <Select isMulti name="problemTags" value={selectedPTags} onChange={setSelectedPTags} options={problemTags}></Select>
         </label>
         <label>
           Contest Tags:
-          <input type="text" name="contestTags" />
+          <Select isMulti name="problemTags" value={selectedCTags} onChange={setSelectedCTags} options={contestTags}></Select>
         </label>
         <button type="submit">Submit</button>
       </form>

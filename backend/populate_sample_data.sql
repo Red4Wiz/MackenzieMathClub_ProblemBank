79dd7bbfcb7f103dc9d4695b0b2e366a53e78cbb
Daniel Ye <danielyeqt@gmail.com>
2023-07-22 09:09:18 -0400

    make sqlite3 single quotes

--- backend/populate_sample_data.sql
+++ backend/populate_sample_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO Users (username, firstname, lastname, password, permission) VALUES ("admin", "Admin", "Wong", "asdf", 1);
-INSERT INTO ProblemTags (name) VALUES ("geometry"), ("algebra"), ("number theory");
-INSERT INTO ContestTags (name) VALUES ("amc"), ("euclid");
+INSERT INTO Users (username, firstname, lastname, password, permission) VALUES ('admin', 'Admin', 'Wong', 'asdf', 1);
+INSERT INTO ProblemTags (name) VALUES ('geometry'), ('algebra'), ('number theory');
+INSERT INTO ContestTags (name) VALUES ('amc'), ('euclid');
